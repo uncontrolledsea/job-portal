6ba586944bba91ebfaf0d86b5350dfa1470e91de
shubham <smore4569@gmail.com>
2026-04-17 13:50:25 +0530

    fix case issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:"http://localhost:5000/api/v1",
+baseURL: "https://job-portal-backend.onrender.com/api/v1"
 });
 api.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
